4f986ac52a7b8826bb83e6c24c4db84bd889a9fe
Canaan-HS <s3459897@gmail.com>
2025-05-10 11:14:15 +0800

    Title 組件

---
+++ HTML/Vue/src/components/Title.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-center text-info">{{ props.title }}</h1>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        title: String
+    });
+</script>
